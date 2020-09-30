fcfc76f14a72594b305f5e8ca389fee73b81861d
pchiaran <72037194+pchiaran@users.noreply.github.com>
2020-09-30 10:27:18 +0200

    Update node.hpp

--- BST_advanced_programming_exam/include/node.hpp
+++ BST_advanced_programming_exam/include/node.hpp
@@ -27,13 +27,13 @@ namespace NODE{
 		node<T>* upper;				//pointer to the parent. Cannot use unique_ptr here
 
 		//user-defined default constructor. It initializes the node value to the default value depending on type T and the three node's pointers to nullptr
-		node() noexcept : value{}, left{nullptr}, right{nullptr}, upper{nullptr} { std::cout << "Node default constructor" << std::endl; } 
+		node() noexcept : value{}, left{nullptr}, right{nullptr}, upper{nullptr} {} 
 		
     		//custom constructor to initialize a node using an lvalue. This copies an exixsting node
-		node(const T& v, node<T>* up) noexcept : value{v}, left{nullptr}, right{nullptr}, upper{up} { std::cout << "Node custom constructor" << std::endl; }
+		node(const T& v, node<T>* up) noexcept : value{v}, left{nullptr}, right{nullptr}, upper{up} {}
 
 		//custom constructor to initialize a node using an rvalue
-		node(T&& v, node<T>* up) noexcept : value{v}, left{nullptr}, right{nullptr}, upper{up} { std::cout << "Node move constructor" << std::endl;  }
+		node(T&& v, node<T>* up) noexcept : value{v}, left{nullptr}, right{nullptr}, upper{up} {}
 
 		//set bst and iterator as friend classes to access node's private members in them
 		template<typename K, typename V, typename cmp>
